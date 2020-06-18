43bddf10ed97de405c4ea665afae09045c9ff932
joaoclopes <joaoclopes14@gmail.com>
2020-06-18 00:32:38 -0300

    alguns htmls

---
+++ routes/index.php
@@ -0,0 +1,18 @@
+<?php
+
+if(array_key_exists('action', $_GET) && array_key_exists('method', $_GET)) {
+    $class = $_GET['action'];
+    $method = $_GET['method'];
+
+        if(!class_exists($class)) {
+            echo 'Não possui classe';
+        }else if(!method_exists(new $class, $method)) {
+            echo 'Não possui metodo';
+            
+        } else {
+            $controller = new $class;
+            $controller->$method();
+        }
+} else {
+    echo 'Não possui valores';
+}
